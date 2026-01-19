eb6f9b3e3213d57b16424c87f2139e0bb98e4b17
rangelvulkov <rangelvulkov1@gmail.com>
2026-01-19 11:42:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
-self.addEventListener('fetch', () => {
-    // Позволява инсталиране
+// Minimal Service Worker за PWA
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('social-compass-store').then((cache) => cache.addAll([
+        './index.html',
+        './manifest.webmanifest',
+      ])),
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
 });
